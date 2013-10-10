89f598ab4f1ec9861d99990ea636b0d277783e2a
Wojtek Erbetowski <wojtek.erbetowski@polidea.com>
2013-10-10 14:37:24 +0200

    Time to start

--- script/countdown-main.js
+++ script/countdown-main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("#count").countdown({
         //to change lunch date just replace the current date with yours .
-        date: "october 12, 2013, 08:00",
+        date: "october 12, 2013, 09:00",
         //html code in count div here.
         htmlTemplate: $('#count').html()
     });
